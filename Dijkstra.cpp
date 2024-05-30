8495c310cbde5fcd94ddb930884ad16dfda55cf4
Mija Pilkaite <115141099+mija-pilkaite@users.noreply.github.com>
2024-05-30 19:25:59 +0200

    adding tests and fixing typos in Dijkstra

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -18,7 +18,7 @@ std::vector<double> dijkstra(const Graph &graph, int source)
 
     std::vector<std::vector<vwPair>> adjacencyList = graph.getAdjacencyList();    //Adjacency list from the graph
 
-    std::priorityQueue<vwPair> priorityQueue; //Priority queue to hold the vertices to be explored
+    std::priority_queue<vwPair, std::vector<vwPair>, std::greater<vwPair>> priorityQueue; //Priority queue to hold the vertices to be explored
 
     // Start with the source vertex
     distances[source] = 0.0;
@@ -51,4 +51,5 @@ std::vector<double> dijkstra(const Graph &graph, int source)
 
     return distances;
 }
+
 
